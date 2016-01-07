7e5f24401b3427fcaa0af847fea2c2026a605ddc
Eruditious <metamanchester@gmail.com>
2016-01-07 13:43:36 -0600

    cleaned up

--- js/app.js
+++ js/app.js
@@ -2,31 +2,14 @@ var app = angular.module('MyApp', ['ngRoute']);
 
 app.config(function($routeProvider) {
   $routeProvider
-
     .when('/', {
       templateUrl : '/js/views/home.html'
     })
-
     .when('/home', {
       templateUrl : '/js/views/home.html'
     })
-
-    .when('/contact', {
-      templateUrl : '/js/views/contact.html'
-
-    })
-    .when('/blog', {
-      templateUrl : '/js/views/blog.html'
-
-    })
     .when('/portfolio', {
       templateUrl: '/js/views/portfolio.html'
-
     })
-    .when('/resume', {
-      templateUrl: 'js/views/resume.html'
-    })
-    .when('/about', {
-      templateUrl: 'js/views/about.html'
-    });
+        
 });
