ede871c70e3eb0e5ec9076993047495178ffd21a
Thomas Manchester <thomasmanchester@pharmetika.com>
2016-09-22 00:36:53 -0500

    added api stuff test

--- js/controllers/maincontroller.js
+++ js/controllers/maincontroller.js
@@ -7,8 +7,23 @@ app.controller('mainCtrl',  function($scope, $http) {
        $scope.blogdata = response;
      });
   }
+$scope.saveRebates = function(rebates) {
+  console.log(rebates);
 
-    
-  
+};
+  $scope.getSomeRebates = function () {
+  	$.ajax({
+  		type: "POST",
+  		url: "https://www.rebatebus.com/api/getrebates",
+  		data: {"uid": 95, "apikey": "5wwE4pRmR4f9lVLm"},
+  		success: function(response, stat) {
+  			saveRebates(response);
+  		}, error: function(response, stat) {
+  			console.log("error retrieving data from Rebate Bus");
+  		}
+  	});
+  }
+$scope.getSomeRebates();
+
 
 });
